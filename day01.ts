d7fba4de2959fdbc91deaaf13b2fbe4ed21f0a80
Céline Bijtenhoorn <celine.bijtenhoorn@rabobank.com>
2024-12-01 15:35:50 +0100

    first commit

---
+++ day01.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from "fs";
+const input = readFileSync("./day01input", "utf-8").split("\n");
+
+//PART ONE
+const leftList = input
+  .map((id) => Number(id.split("   ")[0]))
+  .sort((a, b) => a - b);
+const rightList = input
+  .map((id) => Number(id.split("   ")[1]))
+  .sort((a, b) => a - b);
+
+const differences = leftList.map((leftValue, index) =>
+  Math.abs(leftValue - rightList[index])
+);
+
+const totalDifference = differences.reduce((sum, value) => sum + value, 0);
+
+//PART TWO
+const scores = leftList.map((value) => {
+  const count = rightList.filter((rightValue) => rightValue === value).length;
+  return count * value;
+});
+
+const similarityScore = scores.reduce((sum, value) => sum + value, 0);
+console.log(similarityScore);
